d7ab1f770c01904798bf7517f792fa4e8d5a5cc5
chandradewi26 <chandradewi26@gmail.com>
2020-11-19 21:45:26 +1100

    Adding New Notepad
    Useful Links about Git

---
+++ Introduction to Git.java
@@ -0,0 +1,9 @@
+https://www.notion.so/Introduction-to-Git-ac396a0697704709a12b6a0e545db049#d5e9c2b6379246a593c1ef74051e7e3c
+
+
+
+Git Ignore
+https://github.com/github/gitignore
+https://docs.github.com/en/free-pro-team@latest/github/using-git/ignoring-files
+Common git ignore
+https://gist.github.com/octocat/9257657
